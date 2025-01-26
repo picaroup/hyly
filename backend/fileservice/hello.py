25d6595fad6a70a28c3b3e414f64738455380730
Joker Z <zhanghy000107@gmail.com>
2025-01-25 17:17:27 -0800

    init python projects

---
+++ backend/fileservice/hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fileservice!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ backend/jobservice/hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from jobservice!")
+
+
+if __name__ == "__main__":
+    main()
